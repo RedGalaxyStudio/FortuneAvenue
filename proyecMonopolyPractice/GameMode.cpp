59af14320a4eb9cc74b9195f6c28b2cd533f0ad6
Daniel2324 <danifernandez1004@gmail.com>
2024-10-10 15:21:30 -0400

    Resolved merge conflicts888999995

--- proyecMonopolyPractice/GameMode.cpp
+++ proyecMonopolyPractice/GameMode.cpp
@@ -99,7 +99,7 @@ void GameMode::update() {
     //MarcoPlayers[2].setScale(0.7f, 0.7f);
     AvatarPlayers[2].setPosition(1052.5f, 652.5f);
     AvatarPlayers[2].setScale(0.7f, 0.7f);
-
+    //hola
     //perfil 4
     NamePlayers[3].setPosition(1188.65f, 652.5f);
     boxPlayers[3].setPosition(1188.65f, 652.5f);
@@ -109,7 +109,7 @@ void GameMode::update() {
 
      // Llamar a Juan en un hilo separado
     std::thread hiloCliente(Juan);
-
+   
     AvatarPlayers[3].setPosition(1052.5f, 652.5f);
     AvatarPlayers[3].setScale(0.7f, 0.7f);
 
