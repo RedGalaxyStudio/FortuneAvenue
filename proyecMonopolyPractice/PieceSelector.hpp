84696cd63d08c03eec51c3d6063d4d39cb9681b6
Daniel2324 <danifernandez1004@gmail.com>
2024-10-11 01:53:00 -0400

    listo para presentar

--- proyecMonopolyPractice/PieceSelector.hpp
+++ proyecMonopolyPractice/PieceSelector.hpp
@@ -8,7 +8,10 @@ class PieceSelector {
 private:
     std::vector<sf::Sprite> pieces;  // List of pieces to select
     std::vector<sf::Sprite> shadow;  // List of pieces to select
+    std::vector<sf::Sprite> Check;  // List of pieces to select
     std::vector<sf::Texture> piecesTextures;
+    std::vector<sf::Texture> CheckTexturesOn;
+    std::vector<sf::Texture> CheckTexturesOff;
     int selectedPiece;               // Index of the selected piece
     sf::RenderWindow* window;        // Reference to the window
     sf::Sprite* newSelection;  // Puntero para la nueva selección
@@ -23,7 +26,7 @@ public:
     void displayPieces();
 
     // Update the selection based on user input
-    void updateSelection();
+    sf::Texture updateSelection();
 
     // Get the selected piece
     sf::Sprite& getSelectedPiece();
