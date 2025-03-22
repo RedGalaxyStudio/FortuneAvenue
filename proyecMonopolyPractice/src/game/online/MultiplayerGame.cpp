7489a55125d5e0ed7faff5191da848ce05ec0a77
MarcosJ <marcosr.250806@gmail.com>
2025-03-22 13:53:48 -0400

    ay ya

--- proyecMonopolyPractice/src/game/online/MultiplayerGame.cpp
+++ proyecMonopolyPractice/src/game/online/MultiplayerGame.cpp
@@ -636,11 +636,6 @@ void MultiplayerGame::Event() {
 	sf::Event event;
 	sf::Vector2i mousePosition = sf::Mouse::getPosition(*window);
 	sf::Vector2f mousePosFloat = static_cast<sf::Vector2f>(mousePosition);
-
-	GameEnd gameend(window, client);
-	gameend.resource();
-	gameend.update();
-
 	while (window->pollEvent(event)) {
 	
 			Dado.loop(event, client);
